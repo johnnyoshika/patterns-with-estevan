ae687ba4d682c31e6f447a48f50f125863a86c30
Johnny Oshika <johnnyoshika@gmail.com>
2022-10-04 08:02:06 -0700

    Add adapter pattern example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('hello');
+interface AmericanPower {
+  use110v(): void;
+}
+
+interface EuropeanPower {
+  use220v(): void;
+}
+
+class EuropeanToAmericanPowerAdapter implements AmericanPower {
+  constructor(private europeanPower: EuropeanPower) {}
+
+  use110v(): void {
+    console.log('Converting 220v to 110v');
+    this.europeanPower.use220v();
+  }
+}
+
+class EuropeanAppliance implements EuropeanPower {
+  use220v(): void {
+    console.log('Using 220v');
+  }
+}
+
+const primeraDishwasher = new EuropeanAppliance();
+const powerAdapter = new EuropeanToAmericanPowerAdapter(
+  primeraDishwasher,
+);
+powerAdapter.use110v();
