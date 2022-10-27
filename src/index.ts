d988c9c28cc3189e6483ee33284bbc701daaf3c9
Johnny Oshika <johnnyoshika@gmail.com>
2022-10-26 17:24:43 -0700

    Test in sandbox

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('hello');
+class Person {
+  constructor(
+    public readonly name: string,
+    public readonly age: string = '25',
+    public readonly country: string,
+  ) {}
+}
+
+const john = new Person('John', undefined, 'USA');
+
+console.log('Age', john.age);
