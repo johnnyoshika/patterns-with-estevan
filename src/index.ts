f630b75e5cf015e15ab6c6d62f4cc8e8dd91c6ae
Johnny Oshika <johnnyoshika@gmail.com>
2022-11-01 07:37:11 -0700

    Add decorator pattern example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,60 @@
-console.log('hello');
+abstract class Pizza {
+  abstract description: string;
+  abstract price: number;
+}
+
+class ThinCrustPizza extends Pizza {
+  description = 'Thin Crust';
+  price = 8;
+}
+
+class ThickCrustPizza extends Pizza {
+  description = 'Thick Crust';
+  price = 10;
+}
+
+class FlatBreadPizza extends Pizza {
+  description = 'Flat Bread';
+  price = 12;
+}
+
+abstract class PizzaDecorator extends Pizza {
+  constructor(private pizza: Pizza) {
+    super();
+  }
+
+  abstract decoratorDescription: string;
+  abstract decoratorPrice: number;
+
+  get description() {
+    return `${this.decoratorDescription} ${this.pizza.description}`;
+  }
+
+  get price() {
+    return this.pizza.price + this.decoratorPrice;
+  }
+}
+
+class Cheese extends PizzaDecorator {
+  decoratorDescription = 'Cheese';
+  decoratorPrice = 4;
+}
+
+class Pepperoni extends PizzaDecorator {
+  decoratorDescription = 'Pepperoni';
+  decoratorPrice = 5;
+}
+
+class Pineapple extends PizzaDecorator {
+  decoratorDescription = 'Pineapple';
+  decoratorPrice = 2;
+}
+
+function makePizza(pizza: Pizza) {
+  console.log(
+    `Making a "${pizza.description}" pizza for $${pizza.price}`,
+  );
+}
+
+makePizza(new Pineapple(new Cheese(new ThinCrustPizza())));
+makePizza(new Pepperoni(new Cheese(new ThickCrustPizza())));
