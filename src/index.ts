f738dfec3c000130123e9b7ea6b7a11e2a0d5958
Johnny Oshika <johnnyoshika@gmail.com>
2022-12-13 11:50:07 -0800

    Add iterator pattern example

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log('hello');
+class ArrayIterator<T> {
+  private _array: T[];
+  private _index: number;
+
+  constructor(array: T[]) {
+    this._array = array;
+    this._index = 0;
+  }
+
+  public next(): T {
+    return this._array[this._index++];
+  }
+
+  public hasNext(): boolean {
+    return this._index < this._array.length;
+  }
+}
+
+class User {
+  constructor(public name: string) {}
+
+  public sayHi(): void {
+    console.log(`Hi, I'm ${this.name}`);
+  }
+}
+
+const users = new ArrayIterator([
+  new User('John'),
+  new User('Jane'),
+  new User('Bob'),
+]);
+
+while (users.hasNext()) users.next().sayHi();
